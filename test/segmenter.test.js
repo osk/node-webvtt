c65bf986cab89428954c1c4175a07a302aa4a95e
Ólafur Sverrir Kjartansson <olafurs@oz.com>
2016-02-03 12:36:10 +0000

    Fix bug in segmentation

--- test/segmenter.test.js
+++ test/segmenter.test.js
@@ -339,4 +339,187 @@ f`;
     segmented[2].cues[2].should.deep.equal(parsed.cues[10], 'seg 3, cue 3');
     segmented[2].cues[3].should.deep.equal(parsed.cues[11], 'seg 3, cue 4');
   });
+
+  it('should segment an even longer playlist correctly', () => {
+    const input = `WEBVTT
+
+00:00:01.800 --> 00:00:05.160
+0
+
+00:00:05.400 --> 00:00:07.560
+1
+
+00:00:07.640 --> 00:00:09.600
+2
+
+00:00:09.720 --> 00:00:11.360
+3
+
+00:00:11.430 --> 00:00:13.110
+4
+
+00:00:13.230 --> 00:00:15.430
+5
+
+00:00:15.520 --> 00:00:17.640
+6
+
+00:00:17.720 --> 00:00:19.950
+7
+
+00:00:20.040 --> 00:00:23.760
+8
+
+00:00:23.870 --> 00:00:26.320
+9
+
+00:00:26.400 --> 00:00:28.560
+10
+
+00:00:28.640 --> 00:00:30.870
+11
+
+00:00:30.950 --> 00:00:33.230
+12
+
+00:00:33.320 --> 00:00:35.000
+13
+
+00:00:35.080 --> 00:00:37.400
+14
+
+00:00:37.520 --> 00:00:39.400
+15
+
+00:00:39.520 --> 00:00:42.600
+16
+
+00:00:42.720 --> 00:00:46.000
+17
+
+00:00:46.110 --> 00:00:48.200
+18
+
+00:00:48.280 --> 00:00:49.640
+19
+
+00:00:49.680 --> 00:00:52.600
+20
+
+00:00:52.680 --> 00:00:55.000
+21
+
+00:00:55.400 --> 00:00:56.470
+22
+
+00:00:58.720 --> 00:01:00.110
+23
+
+00:01:03.160 --> 00:01:04.200
+24
+
+00:01:43.840 --> 00:01:46.800
+25
+
+00:01:50.430 --> 00:01:53.110
+26
+
+00:01:54.840 --> 00:01:56.160
+27
+
+00:01:58.470 --> 00:02:02.840
+28
+
+00:02:03.840 --> 00:02:05.560
+29
+
+00:02:06.760 --> 00:02:07.720
+30
+
+00:02:07.800 --> 00:02:11.280
+31
+
+00:02:11.400 --> 00:02:14.320
+32
+
+00:02:14.430 --> 00:02:15.470
+33
+
+00:02:15.600 --> 00:02:17.920
+34
+
+00:02:18.000 --> 00:02:20.520
+35
+
+00:02:20.600 --> 00:02:23.110
+36
+
+00:02:23.200 --> 00:02:25.840
+37
+
+00:02:25.950 --> 00:02:29.800
+38
+
+00:02:29.870 --> 00:02:31.230
+39
+
+00:02:31.320 --> 00:02:33.280
+40
+
+00:02:33.560 --> 00:02:37.160
+41
+
+00:02:37.360 --> 00:02:39.560
+42`;
+
+    const parsed = parse(input);
+    const segmented = segment(input);
+
+    parsed.cues.should.have.length(43, 'parsed cues');
+    segmented.should.have.length(13, 'segments');
+
+    segmented[0].duration.should.equal(10, 'seg 1 duration');
+    segmented[0].cues.length.should.equal(4, 'seg 1 count');
+
+    segmented[1].duration.should.equal(10, 'seg 2 duration');
+    segmented[1].cues.length.should.equal(5, 'seg 2 count');
+
+    segmented[2].duration.should.equal(10, 'seg 3 duration');
+    segmented[2].cues.length.should.equal(4, 'seg 3 count');
+
+    segmented[3].duration.should.equal(10, 'seg 4 duration');
+    segmented[3].cues.length.should.equal(6, 'seg 4 count');
+
+    segmented[4].duration.should.equal(10, 'seg 5 duration');
+    segmented[4].cues.length.should.equal(5, 'seg 5 count');
+
+    segmented[5].duration.should.equal(10, 'seg 6 duration');
+    segmented[5].cues.length.should.equal(4, 'seg 6 count');
+    segmented[5].cues[0].should.deep.equal(parsed.cues[20], 'seg 6, cue 1');
+
+    segmented[6].duration.should.equal(10, 'seg 7 duration');
+    segmented[6].cues.length.should.equal(2, 'seg 7 count');
+
+    segmented[7].duration.should.equal(40, 'seg 8 duration');
+    segmented[7].cues.length.should.equal(1, 'seg 8 count');
+    segmented[7].cues[0].should.deep.equal(parsed.cues[25], 'seg 8, cue 1');
+
+    segmented[8].duration.should.equal(10, 'seg 9 duration');
+    segmented[8].cues.length.should.equal(3, 'seg 9 count');
+    segmented[8].cues[0].should.deep.equal(parsed.cues[26], 'seg 8, cue 1');
+
+    segmented[9].duration.should.equal(10, 'seg 10 duration');
+    segmented[9].cues.length.should.equal(4, 'seg 10 count');
+
+    segmented[10].duration.should.equal(10, 'seg 11 duration');
+    segmented[10].cues.length.should.equal(5, 'seg 11 count');
+
+    segmented[11].duration.should.equal(10, 'seg 12 duration');
+    segmented[11].cues.length.should.equal(5, 'seg 12 count');
+
+    segmented[12].duration.should.equal(9.56, 'seg 13 duration');
+    segmented[12].cues.length.should.equal(4, 'seg 13 count');
+  });
+
+
 });
